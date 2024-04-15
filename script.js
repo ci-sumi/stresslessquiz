cd4dbfe2794687dac91c2b238dcaad14a8bc09d8
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-04-15 19:19:11 +0100

    Add JavaScript functionality to show rules box on button click

---
+++ script.js
@@ -0,0 +1,7 @@
+// start Button click
+document.getElementById("myButton").addEventListener("click",function(){
+
+    var rulesBox = document.querySelector(".rules");
+    rulesBox.style.display="block"
+
+});
