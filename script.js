9e9760ea99883a19c4e2f1c03862e4a1ef79aaa0
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-05-08 05:26:01 +0100

    Add functionality to display next question in quiz

--- script.js
+++ script.js
@@ -41,16 +41,56 @@ function setPlaceholder(){
 }
 
 
-document.getElementById("nextQuestion").addEventListener("click",function(){
-    var option = document.querySelector(".quiz-options-container")
-    option.style.display="flex"
-    var nextQuestion = document.getElementById("nextQuestion");
-    nextQuestion.style.display = "none"
+// document.getElementById("nextQuestion").addEventListener("click",function(){
+//     var option = document.querySelector(".quiz-options-container")
+//     option.style.display="flex"
+    // var nextQuestion = document.getElementById("nextQuestion");
+    // nextQuestion.style.display = "none"
 
-})
+// })
 function closeOptionModal(){
     var closeOptionModal = document.querySelector(".quiz-options-container")
     closeOptionModal.style.display="none"
-    var nextQuestion = document.getElementById("nextQuestion");
-    nextQuestion.style.display = "block"
+    // var nextQuestion = document.getElementById("nextQuestion");
+    // nextQuestion.style.display = "block"
 }
+
+//Define Array of quiz questions
+const quizQuestion = [
+    {
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Rome"],
+    answer: "Paris"
+    },
+    {
+    question: "what is the largest planet in our solar system?",
+    options: ["Earth","Jupitar","Mars","Saturn"],
+    answer: "Jupitar"
+    },
+];
+
+let currentQuestionIndex = 0;
+const displayQuestion = document.getElementById("display-question");
+const optionOneLabel = document.getElementById("option-one-label");
+const optionTwoLabel = document.getElementById("option-two-label");
+const optionThreeLabel = document.getElementById("option-three-label");
+const optionFourLabel = document.getElementById("option-four-label");
+
+function renderQuestion(){
+    const currentQuestion = quizQuestion[currentQuestionIndex];
+    displayQuestion.textContent = currentQuestion.question;
+    optionOneLabel.textContent = currentQuestion.options[0];
+    optionTwoLabel.textContent = currentQuestion.options[1];
+    optionThreeLabel.textContent = currentQuestion.options[2];
+    optionFourLabel.textContent = currentQuestion.options[3];
+}
+nextQuestion.addEventListener("click",()=>{
+    currentQuestionIndex++;
+    if(currentQuestionIndex<quizQuestion.length){
+        renderQuestion();
+    }else{
+        console.log("End  the quiz")
+    }
+})
+renderQuestion()
+
